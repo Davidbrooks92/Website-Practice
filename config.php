e6f1c0e4be96e529ee9fdb30388f8d381342682c
davesweb <Davidbrooks92@hotmail.com>
2022-03-11 19:01:09 +0000

    tshrthsrtfh

--- config.php
+++ config.php
@@ -52,16 +52,12 @@ function template_footer()
     
 </body>
 <footer class="footer">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10234.11845083172!2d-5.2693913!3d50.1138109!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x656537ff3a72993b!2sCornwall%20Auto%20Technics%20Ltd!5e0!3m2!1sen!2suk!4v1644421880679!5m2!1sen!2suk" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-
-        <p>7 Water-Ma-Trout, Helston Cornwall TR13 0LW</p>
-        <p>Tel: 01326 554063</p>
-      </article>
-      <aside>
-               
-     
+  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10234.11845083172!2d-5.2693913!3d50.1138109!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x656537ff3a72993b!2sCornwall%20Auto%20Technics%20Ltd!5e0!3m2!1sen!2suk!4v1644421880679!5m2!1sen!2suk" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy">
+    </iframe>
+    <p id="copy">7 Water-Ma-Trout, Helston Cornwall TR13 0LW</p>
+    <p id="copy">Tel: 01326 554063</p>     
     <p id="copy">Copyright &#169; $year <a href="/">Cornwall Auto Technics</a></p>
-<p id="time"></p> 
+    <p id="time"></p> 
 </footer>
 </html>
 EOT;
