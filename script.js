fb12ebfbf92376bc3aecc9755b2a50245f832824
davesweb <Davidbrooks92@hotmail.com>
2022-03-15 16:12:25 +0000

    fdhzrdgz

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
-const items = document.querySelectorAll(".item");
-
+ 
 /* Toggle mobile menu */
 function toggleMenu() {
     if (menu.classList.contains("active")) {
@@ -20,26 +19,29 @@ function toggleMenu() {
 /* Event Listener */
 toggle.addEventListener("click", toggleMenu, false);
 
+const items = document.querySelectorAll(".item");
+ 
 /* Activate Submenu */
 function toggleItem() {
-    if (this.classList.contains("submenu-active")) {
-      this.classList.remove("submenu-active");
-    } else if (menu.querySelector(".submenu-active")) {
-      menu.querySelector(".submenu-active").classList.remove("submenu-active");
-      this.classList.add("submenu-active");
-    } else {
-      this.classList.add("submenu-active");
-    }
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
   }
-   
-  /* Event Listeners */
-  for (let item of items) {
-      if (item.querySelector(".submenu")) {
-        item.addEventListener("click", toggleItem, false);
-        item.addEventListener("keypress", toggleItem, false);
-      }   
-  }
-  /* Close Submenu From Anywhere */
+}
+ 
+/* Event Listeners */
+for (let item of items) {
+    if (item.querySelector(".submenu")) {
+      item.addEventListener("click", toggleItem, false);
+      item.addEventListener("keypress", toggleItem, false);
+    }   
+}
+
+/* Close Submenu From Anywhere */
 function closeSubmenu(e) {
     let isClickInside = menu.contains(e.target);
    
