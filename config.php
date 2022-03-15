bc67b2abfa28c416ce93f780731a9674e71158cd
davesweb <Davidbrooks92@hotmail.com>
2022-03-15 16:18:46 +0000

    s\gwsg

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <script src="script.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script type="text/javascript">
   var timestamp = '<?= time(); ?>';
