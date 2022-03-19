e742ca6a248110dae860d977c79fca2fbaa2eded
davesweb <Davidbrooks92@hotmail.com>
2022-03-19 16:33:16 +0000

    Index update

--- index.php
+++ index.php
@@ -5,7 +5,11 @@ include "config.php";
 
 <?= template_header('Home') ?>
 <div class="mainbody">
-    <p>Hello</p>
+    <h1>Welcome to Cornwall Auto Technics</h1>
+    <p>This is a test website</p>
+    <p>If you would like to contact Cornwall Auto Technics</p>
+    <p>Please visit <a href="www.cornwallautotechnics.co.uk">www.cornwallautotechnics.co.uk</a></p>
+
 </div>
 
 <?= template_footer() ?>
