e624b2a97232ca0170dd13b0ec404e4ca9a976f1
davesweb <Davidbrooks92@hotmail.com>
2022-03-19 17:17:44 +0000

    styling update

--- aboutus.php
+++ aboutus.php
@@ -6,6 +6,19 @@ include "config.php";
 <?= template_header('About Us') ?>
 
 <div class="mainbody">
-
+    <h1>About Us</h1>
+    <div class="subbody">
+        <div class="subsubbody">
+            <h2>A Friendly Face</h2>
+            <p>We specialise in all things electronic for your car but also undertake a wide range of repair and improvement.
+                So feel free to drop in to chat and discuss your requirements at your convenience.</p>
+        </div>
+        <div class="subsubbody">
+            <h2>We're Here To Help</h2>
+            <p>Our team of mechanics and electrical specialists ensures a high standard of work in a time scale and price that suits you.
+                Come in or call for a chat about your needs, or email us anytime.
+            </p>
+        </div>
+    </div>
 </div>
 <?= template_footer() ?>
