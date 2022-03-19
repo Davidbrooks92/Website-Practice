7a92c64af64eb5734d5b0aac9a0a5f7e6bdfb5a0
davesweb <Davidbrooks92@hotmail.com>
2022-03-19 16:51:18 +0000

    Services Update

--- services.php
+++ services.php
@@ -4,6 +4,54 @@ include "config.php";
 ?>
 
 <?= template_header('Services') ?>
-
+<div>
+    <ul>
+        <li>
+            Our Services
+        </li>
+        <li>
+            MOTs
+        </li>
+        <li>
+            Performance & Economy Re-Mapping
+        </li>
+        <li>
+            Complete Computer Diagnostics
+        </li>
+        <li>
+            Electrical Systems
+        </li>
+        <li>
+            ECU Repairs
+        </li>
+        <li>
+            Hands-Free System Supply & Install
+        </li>
+        <li>
+            Parking Sensor Supply & Install
+        </li>
+        <li>
+            Battery Supply & Install
+        </li>
+        <li>
+            Starter Motor Supply & Install
+        </li>
+        <li>
+            Alternator Supply & Install
+        </li>
+        <li>
+            Window & Door Locking Servicing
+        </li>
+        <li>
+            Tow Bar & Electrical Servicing
+        </li>
+        <li>
+            Audio System Supply & Install
+        </li>
+        <li>
+            Alarm & Immobilliser Maintinence
+        </li>
+    </ul>
+</div>
 
 <?= template_footer() ?>
